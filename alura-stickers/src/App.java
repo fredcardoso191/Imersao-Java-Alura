0d03e866aa3763c7e44ed2712dc99d9600376dcb
Fred Cardoso <fredcardoso01@hotmail.com>
2022-07-18 21:20:06 -0300

    add user rating

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -3,21 +3,32 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
 public class App {
     private static JsonParser jsonParser;
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        System.out.println("1 - Most Popular Movies. | 2 - Top 250 Movies.");
-        System.out.print("Enter a valid value: ");
-        int value = sc.nextInt();
 
         //create http connection//search top 250 movies
-        String url = (value == 1) ? "https://imdb-api.com/en/API/MostPopularMovies/(Your API key here)" :
-                "https://imdb-api.com/en/API/Top250Movies/(Your API key here)";
-
+        String url = "";
+        while (true){
+            System.out.println("1 - Most Popular Movies. | 2 - Top 250 Movies.");
+            System.out.print("Enter a valid value: ");
+            int value = sc.nextInt();
+            if (value == 1) {
+                url = "https://imdb-api.com/en/API/MostPopularMovies/(Your API key here)";
+                break;
+            } else if (value == 2) {
+                url = "https://imdb-api.com/en/API/Top250Movies/(Your API key here)";
+                break;
+            } else {
+                System.out.println("Invalid value.");
+            }
+        }
         //API Top 250 Movies reserve: https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060
 
         URI uri = URI.create(url);
@@ -35,9 +46,9 @@ public class App {
             try {
                 System.out.println("\nTitle: " + movie.get("title"));
                 System.out.println("Image: " + movie.get("image"));
-                System.out.println("Rating: " + movie.get("imDbRating"));
                 double rating = Double.parseDouble(movie.get("imDbRating"));
-                System.out.print("Star Rating: ");
+                System.out.print("Rating: ");
+                //System.out.print("Star Rating: ");
                 for (double i = 0; i < Math.round(rating); i++) {
                     System.out.print("\u2606");
                     //System.out.print("\u2B50");
@@ -47,6 +58,24 @@ public class App {
                 System.out.print("Invalid format.");
             }
             System.out.println("\n");
+        }
+
+        //User Rating
+        System.out.println("Want to rate a movie?");
+        System.out.print("Enter a value (Y/N): ");
+        sc.nextLine();
+        char op = sc.nextLine().charAt(0);
+        if (op == 'y') {
+            System.out.print("Enter movie name: ");
+            String name = sc.nextLine();
+            System.out.print("Enter a rating (0-10): ");
+            double av = sc.nextDouble();
+            System.out.println("User rating for the movie " + name);
+            //System.out.println("\nTitle: " + name);
+            System.out.print("Rating: ");
+            for (int i = 0; i < Math.round(av); i++) {
+                System.out.print("\u2606");
+            }
         }
         sc.close();
     }
